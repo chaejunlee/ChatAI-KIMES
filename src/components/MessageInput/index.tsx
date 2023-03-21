09c02b9ed2dbf50d5480b8d914cfb01e34a2679b
BEOKS <lee01042000@gmail.com>
2023-03-21 12:03:11 +0900

    Feat : MessageInput

--- src/components/MessageInput/index.tsx
+++ src/components/MessageInput/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import {IconButton, Paper, TextField} from "@mui/material";
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
-export default function MessageInput(){
+
+interface MessageInputProps {
+    onClick: (message: string) => void
+}
+export default function MessageInput({onClick}: MessageInputProps) {
+    const [text, setText] = React.useState<string>('')
+
     return(
         <Paper>
-            <TextField/>
-            <IconButton><InsertEmoticonIcon/></IconButton>
+            <TextField onChange={v=>setText(v.target.value)}/>
+            <IconButton onClick={(e)=>onClick(text)}><InsertEmoticonIcon/></IconButton>
         </Paper>
     )
+}
+
+MessageInput.defaultProps = {
+    onClick: (message: string) => {
+        console.log(message)
+    }
 }
