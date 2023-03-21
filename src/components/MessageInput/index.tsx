cd8561e4441589770f7479e49699a7ab4e4ab471
Henry-Hong <hhr8540@naver.com>
2023-03-21 16:50:20 +0900

    Style : 텍스트 인풋 디자인 변경
    Todo
    1. 엔터시 메시지 전송
    2. 보더 정리

--- src/components/MessageInput/index.tsx
+++ src/components/MessageInput/index.tsx
@@ -1,23 +1,43 @@
 import React from "react";
-import {IconButton, Paper, TextField} from "@mui/material";
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import { IconButton, InputAdornment, Paper, TextField } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import SendIcon from "@mui/icons-material/Send";
 
 interface MessageInputProps {
-    onClick: (message: string) => void
+	onClick: (message: string) => void;
 }
-export default function MessageInput({onClick}: MessageInputProps) {
-    const [text, setText] = React.useState<string>('')
 
-    return(
-        <Paper>
-            <TextField onChange={v=>setText(v.target.value)} label={"Message..."}/>
-            <IconButton onClick={(e)=>onClick(text)}><InsertEmoticonIcon/></IconButton>
-        </Paper>
-    )
+export default function MessageInput({ onClick }: MessageInputProps) {
+	const [text, setText] = React.useState<string>("");
+
+	return (
+		<Paper>
+			<TextField
+				// TODO: enter 누를때 메시지 전송로직
+				onChange={(v) => setText(v.target.value)}
+				placeholder="Message..."
+				label={""}
+				InputProps={{
+					startAdornment: (
+						<InputAdornment position="start">
+							<AddCircleIcon />
+						</InputAdornment>
+					),
+					endAdornment: (
+						<InputAdornment position="end">
+							<IconButton onClick={(e) => onClick(text)}>
+								<SendIcon />
+							</IconButton>
+						</InputAdornment>
+					),
+				}}
+			/>
+		</Paper>
+	);
 }
 
 MessageInput.defaultProps = {
-    onClick: (message: string) => {
-        console.log(message)
-    }
-}
+	onClick: (message: string) => {
+		console.log(message);
+	},
+};
