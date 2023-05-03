2c6e395d20a03c0e25000f57bdc0e1752af85c3c
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-05-03 15:37:48 +0900

    Fix: `%` 단위를 `rem`으로 수정

--- src/components/MessageInput/index.tsx
+++ src/components/MessageInput/index.tsx
@@ -59,7 +59,7 @@ export default function MessageInput({ onClick, loading }: MessageInputProps) {
 	return (
 		<TextField
 			sx={{
-				width: "95%",
+				width: "calc(100% - 2rem)",
 				borderRadius: "200px",
 				position: "absolute",
 				bottom: "1.5rem",
