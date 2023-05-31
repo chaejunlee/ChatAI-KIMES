a8c4d5a913fc02b03bb884f282a1a809fd462d88
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-05-31 17:17:19 +0900

    Fix: change to using EntityAdapter

--- src/store/message/messageSlice.ts
+++ src/store/message/messageSlice.ts
@@ -1,49 +1,79 @@
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { EntityId, PayloadAction, createEntityAdapter } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 import { introMessage } from "../../Data/Message";
-import Message from "../../Interface/Message/Message";
-import { ResponseMessageType } from "../../Interface/Message/ResponseMessageType";
 import { createRequest } from "../../utils/Message/createRequest";
 import { fetchResponse } from "./fetchResponse";
 import { errorMessage } from "../../utils/Message/errorMessageContent";
+import { RootState } from "../store";
+import { ResponseMessageType } from "../../Interface/Message/ResponseMessageType";
+import RequestMessageType from "../../Interface/Message/RequestMessageType";
+
+export interface MessageWithId {
+	content: ResponseMessageType | RequestMessageType;
+	id: number;
+}
 
 interface MessageState {
-	data: Message[];
 	status: "idle" | "loading" | "failed" | "succeeded";
 }
 
-const initialState: MessageState = {
-	data: [introMessage],
+const messageAdapter = createEntityAdapter<MessageWithId>();
+
+const initialState = messageAdapter.getInitialState<MessageState>({
 	status: "idle",
-};
+});
+
+const initialStateWithIntroMessage = messageAdapter.addOne(initialState, {
+	id: 0,
+	content: introMessage,
+});
 
 export const messageSlice = createSlice({
 	name: "message",
-	initialState,
+	initialState: initialStateWithIntroMessage,
 	reducers: {
 		addMessage: (state, action: PayloadAction<string>) => {
-			state.data.push(createRequest(action.payload));
+			messageAdapter.addOne(state, {
+				id: createId(state.ids),
+				content: createRequest(action.payload),
+			});
 		},
 	},
 	extraReducers: (builder) => {
 		builder
 			.addCase(fetchResponse.fulfilled, (state, action) => {
 				state.status = "succeeded";
-				state.data.push(action.payload);
+				messageAdapter.addOne(state, {
+					id: createId(state.ids),
+					content: action.payload,
+				});
 			})
 			.addCase(fetchResponse.pending, (state) => {
 				state.status = "loading";
 			})
 			.addCase(fetchResponse.rejected, (state) => {
 				state.status = "failed";
-				state.data.push({
-					type: "response",
-					content: errorMessage,
-				} as ResponseMessageType);
+				messageAdapter.addOne(state, {
+					id: createId(state.ids),
+					content: {
+						type: "response",
+						content: errorMessage,
+					},
+				} as MessageWithId);
 			});
 	},
 });
 
 export const { addMessage } = messageSlice.actions;
 
+export const {
+	selectAll: selectAllMessages,
+	selectById: selectMessageById,
+	selectIds: selectMessageIds,
+} = messageAdapter.getSelectors<RootState>((state) => state.messages);
+
 export default messageSlice.reducer;
+
+const createId = (data: EntityId[]) => {
+	return data.length;
+};
