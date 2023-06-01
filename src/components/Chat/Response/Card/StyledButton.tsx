a494dd150c8ec874e92faaa701056cf36043ad49
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-06-01 23:58:54 +0900

    Fix: styling of active message button

--- src/components/Chat/Response/Card/StyledButton.tsx
+++ src/components/Chat/Response/Card/StyledButton.tsx
@@ -18,8 +18,8 @@ export const StyledButton = styled("button")<IsSelectedInterface>`
 	min-width: 50px; !important;
 
 	margin: 0 !important;
-	padding-block: 0.5rem;
-	padding-inline: 0.8rem;
+	padding-block: calc(0.6rem - ${Style("2px", "1px")});
+	padding-inline: calc(0.9rem - ${Style("2px", "1px")});
 
 	background: ${Style("white", "white")};
 	color: ${Style(primaryColor, "#6a6a6a")};
@@ -28,12 +28,12 @@ export const StyledButton = styled("button")<IsSelectedInterface>`
 	font-size: 1rem;
 
 	border-radius: 1.25rem;
-	border: ${Style("3px", "1px")} solid ${Style(primaryColor, "#bed1d1")};
-	margin: ${Style("-2px", "0px")};
+	border: ${Style("2px", "1px")} solid ${Style(primaryColor, "#bed1d1")};
+	// margin: ${Style("-2px", "-1px")} !important;
 
 	cursor: pointer;
 
-	transition: all 0.2s ease-in-out;
+	transition: all 0.2s linear;
 
 	&:hover {
 		background: ${Style("#f3f3f3", "#f3f3f3")};
