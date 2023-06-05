637d7dfedaf348861c59f34d78d980b5f8c5ab93
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-06-05 15:23:18 +0900

    Refactor: extract ResponseLink to its own component

--- src/components/Chat/Response/ContentResponseMessage.tsx
+++ src/components/Chat/Response/ContentResponseMessage.tsx
@@ -23,47 +23,51 @@ export default function ContentResponseMessage({
 	message: ContentResponseMessageType;
 }) {
 	if (containsUrl(message.content))
-		return (
-			<div className="message" style={{ marginBottom: "8px" }}>
-				<StyledButton
-					disabled={false}
-					style={{
-						border: "none",
-						filter: "drop-shadow(rgba(150, 200, 150, 0.8) 0px 3px 3px)",
-					}}
-				>
-					<StyledLink
-						style={{ color: `${primaryColor}` }}
-						href={message.content}
-						target="_blank"
-					>
-						<span>관련 링크로 바로가기</span>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							width="1.25rem"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-							/>
-						</svg>
-					</StyledLink>
-				</StyledButton>
-			</div>
-		);
+		return <ResponseLink messageContent={message.content} />;
 
 	return (
 		<BasicResponseMessage>
-			{message.content.split(/\\n|\n/g).map((p, idx) => (
-				<Typography key={p + idx} sx={{ textAlign: "left" }}>
-					{p}
+			{message.content.split(/\\n|\n/g).map((paragraph, idx) => (
+				<Typography key={paragraph + idx} sx={{ textAlign: "left" }}>
+					{paragraph}
 				</Typography>
 			))}
 		</BasicResponseMessage>
+	);
+}
+
+function ResponseLink({ messageContent }: { messageContent: string }) {
+	return (
+		<div className="message" style={{ marginBottom: "8px" }}>
+			<StyledButton
+				disabled={false}
+				style={{
+					border: "none",
+					filter: "drop-shadow(rgba(150, 200, 150, 0.8) 0px 3px 3px)",
+				}}
+			>
+				<StyledLink
+					style={{ color: `${primaryColor}` }}
+					href={messageContent}
+					target="_blank"
+				>
+					<span>관련 링크로 바로가기</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						width="1.25rem"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+						/>
+					</svg>
+				</StyledLink>
+			</StyledButton>
+		</div>
 	);
 }
