47e5e1c72262cdce212ca93b1ba7c0b6a660271f
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-06-13 16:40:53 +0900

    Refactor: ResponseLink - create wrapping component

--- src/components/Chat/Response/ResponseLink.tsx
+++ src/components/Chat/Response/ResponseLink.tsx
@@ -19,25 +19,13 @@ export function ResponseLink({ messageContent }: { messageContent: string }) {
 					target="_blank"
 				>
 					<span>관련 링크로 바로가기</span>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth={1.5}
-						stroke="currentColor"
-						width="20px"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-						/>
-					</svg>
+					<LinkSVG />
 				</StyledLink>
 			</StyledButton>
 		</div>
 	);
 }
+
 export const StyledLink = styled("a")`
 	color: #6a6a6a;
 
@@ -49,3 +37,22 @@ export const StyledLink = styled("a")`
 
 	text-decoration: none;
 `;
+
+function LinkSVG() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={1.5}
+			stroke="currentColor"
+			width="20px"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+			/>
+		</svg>
+	);
+}
