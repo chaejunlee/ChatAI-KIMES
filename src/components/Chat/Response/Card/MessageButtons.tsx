cab4ff62e7956c8ad3efc65efbcd4ae9da6608a3
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-06-14 16:00:20 +0900

    Fix: remove React.memo - it is never cached

--- src/components/Chat/Response/Card/MessageButtons.tsx
+++ src/components/Chat/Response/Card/MessageButtons.tsx
@@ -1,5 +1,4 @@
 import { EntityId } from "@reduxjs/toolkit";
-import { memo } from "react";
 import { selectById } from "../../../../store/message/buttonsSlice";
 import { fetchResponse } from "../../../../store/message/fetchResponse";
 import { isMessageStatusLoading } from "../../../../store/message/messageSlice";
@@ -8,46 +7,44 @@ import { ANIMATION_TARGET } from "../../../../utils/Message/AnimationScope";
 import { ErrorMessage } from "../../ErrorMessage";
 import StyledButton from "./StyledButton";
 
-const MessageButton = memo(
-	({
-		buttonId,
-		addClickedBtn,
-		checkBtnClicked,
-	}: {
-		buttonId: EntityId;
-		addClickedBtn: (buttonIndentifier: string) => void;
-		checkBtnClicked: (buttonIndentifier: string) => boolean;
-	}) => {
-		const dispatch = useAppDispatch();
-		const isLoading = useAppSelector(isMessageStatusLoading);
-		const buttonContent = useAppSelector((state) =>
-			selectById(state.buttons, buttonId)
+const MessageButton = ({
+	buttonId,
+	addClickedBtn,
+	checkBtnClicked,
+}: {
+	buttonId: EntityId;
+	addClickedBtn: (buttonIndentifier: string) => void;
+	checkBtnClicked: (buttonIndentifier: string) => boolean;
+}) => {
+	const dispatch = useAppDispatch();
+	const isLoading = useAppSelector(isMessageStatusLoading);
+	const buttonContent = useAppSelector((state) =>
+		selectById(state.buttons, buttonId)
+	);
+
+	const buttonIndentifier = buttonId.toString();
+	const isClicked = checkBtnClicked(buttonIndentifier);
+
+	if (!buttonContent) return <ErrorMessage />;
+
+	const handleButtonClick = () => {
+		if (isLoading) return;
+		addClickedBtn(buttonIndentifier);
+		dispatch(
+			fetchResponse({ message: buttonContent.value, leaveMessage: false })
 		);
+	};
 
-		const buttonIndentifier = buttonId.toString();
-		const isClicked = checkBtnClicked(buttonIndentifier);
-
-		if (!buttonContent) return <ErrorMessage />;
-
-		const handleButtonClick = () => {
-			if (isLoading) return;
-			addClickedBtn(buttonIndentifier);
-			dispatch(
-				fetchResponse({ message: buttonContent.value, leaveMessage: false })
-			);
-		};
-
-		return (
-			<StyledButton
-				className={ANIMATION_TARGET}
-				disabled={isClicked}
-				id={buttonIndentifier}
-				onClick={handleButtonClick}
-			>
-				{buttonContent.text}
-			</StyledButton>
-		);
-	}
-);
+	return (
+		<StyledButton
+			className={ANIMATION_TARGET}
+			disabled={isClicked}
+			id={buttonIndentifier}
+			onClick={handleButtonClick}
+		>
+			{buttonContent.text}
+		</StyledButton>
+	);
+};
 
 export default MessageButton;
